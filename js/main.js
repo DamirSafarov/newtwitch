43278c87ed1c22c80de6b0b354b0779959ff6ed9
Damir_Safarov <91431110+DamirSafarov@users.noreply.github.com>
2021-12-14 14:42:12 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+$('.left-bar__open').on('click', function () {
+    $('.left-bar').toggleClass('left-bar_hidden');
+    $('.video-player').toggleClass('active1');
+
+})
+
+
+
+$('.right-bar__open').on('click', function () {
+    $('.chat-bar').toggleClass('right-bar_hidden');
+    $('.video-player').toggleClass('active2')
+
+
+})
+
+
+
